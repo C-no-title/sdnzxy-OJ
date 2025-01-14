82225efbf84361c676fcdfd27b031e1f84e6eed3
C-no-title <3129874129@qq.com>
2025-01-14 09:55:00 +0800

    Update d.c

--- 8.指针/问题 D 又见回文/d.c
+++ 8.指针/问题 D 又见回文/d.c
@@ -25,7 +25,7 @@ int main(void)
 
         n=strlen(pstr);
         flag=1;
-        for(i=0;i!=n-1-i&&i!=n-i;i++)
+        for(i=0;i<=n/2-1;i++)
         {
             if(pstr[i]!=pstr[n-1-i])
             {
